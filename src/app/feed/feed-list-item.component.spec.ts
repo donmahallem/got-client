866b205af8a7a406c1acf4e5494394c962347a06
donmahallem <m@m.m0k.eu>
2017-04-12 13:55:54 +0200

    More tests

--- src/app/feed/feed-list-item.component.spec.ts
+++ src/app/feed/feed-list-item.component.spec.ts
@@ -1,7 +1,8 @@
 import {
     TestBed,
     async,
-    ComponentFixture
+    ComponentFixture,
+    inject
 } from '@angular/core/testing';
 import {
     DebugElement,
@@ -36,6 +37,12 @@ import {
 import {
     RedditSubmission
 } from "./../models/reddit-submission.model";
+import {
+    click
+} from "./../../testing/";
+import {
+    Router
+} from "@angular/router";
 
 @Component({
     template: "<feed-list-item  [submission]=\"submission\"></feed-list-item>",
@@ -44,10 +51,16 @@ class TestHostComponent {
     submission: RedditSubmission = {
         title: "[store] test title",
         created_utc: 2999,
-        name: "t3_abcde"
+        name: "t3_abcde",
+        id: "abcde"
     }
 }
-
+class RouterStub {
+    navigate(url: string[]): Promise<boolean> {
+        console.log("tried ", url);
+        return Promise.resolve(true);
+    }
+}
 describe('FeedListItemComponent', () => {
     let testHost: TestHostComponent;
     let testHostFixture: ComponentFixture<TestHostComponent>;
@@ -61,7 +74,12 @@ describe('FeedListItemComponent', () => {
             ], imports: [
                 RouterTestingModule,
                 MaterialModule
-            ]
+            ],
+            providers: [
+                {
+                    provide: Router,
+                    useClass: RouterStub
+                }]
         }).compileComponents();
         testHostFixture = TestBed.createComponent(TestHostComponent);
 
@@ -69,15 +87,14 @@ describe('FeedListItemComponent', () => {
         testHostFixture.detectChanges();
     }));
 
-
-    it("should have as title '[store] test title'", async(() => {
+    it("should have as title '[store] test title'", () => {
         let titleDebugElement: DebugElement = testHostFixture.debugElement.query(By.css('h4'));
         expect(titleDebugElement.nativeElement.textContent).toEqual("[store] test title");
-    }));
+    });
 
-
-    it("should have store as icon", async(() => {
+    it("should have store as icon", () => {
         let iconDebugElement: DebugElement = testHostFixture.debugElement.query(By.css('md-icon'));
         expect(iconDebugElement.nativeElement.textContent).toEqual("store");
-    }));
+    });
+
 });
