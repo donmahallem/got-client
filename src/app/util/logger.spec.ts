6ef26e157f6bf6b173462eaf2760bf5da96a9bb3
donmahallem <m@m.m0k.eu>
2017-04-13 11:34:40 +0200

    added logger tests

---
+++ src/app/util/logger.spec.ts
@@ -0,0 +1,39 @@
+import { Logger } from "./logger";
+import { environment } from "./../environment";
+describe("Logger", () => {
+    const testArg: string[] = ["a", "b", "c"];
+    describe("log()", () => {
+        let spy: jasmine.Spy;
+        beforeEach(() => {
+            spy = spyOn(console, "log");
+        });
+        it("should output not in production", () => {
+            environment.production = false;
+            Logger.log("a", "b", "c");
+            expect(spy.calls.count()).toEqual(1);
+            expect(spy.calls.argsFor(0)).toEqual(testArg);
+        });
+        it("should not output in production", () => {
+            environment.production = true;
+            Logger.log("a", "b", "c");
+            expect(spy.calls.count()).toEqual(0);
+        });
+    });
+    describe("info()", () => {
+        let spy: jasmine.Spy;
+        beforeEach(() => {
+            spy = spyOn(console, "info");
+        });
+        it("should output not in production", () => {
+            environment.production = false;
+            Logger.info("a", "b", "c");
+            expect(spy.calls.count()).toEqual(1);
+            expect(spy.calls.argsFor(0)).toEqual(testArg);
+        });
+        it("should not output in production", () => {
+            environment.production = true;
+            Logger.info("a", "b", "c");
+            expect(spy.calls.count()).toEqual(0);
+        });
+    });
+});
